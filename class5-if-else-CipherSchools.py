33f680a120e881dd834831fa10eedee1a4802745
akriti-star <sharmaakriti8678@gmail.com>
2022-11-29 10:48:24 +0530

    project

---
+++ class5-if-else-CipherSchools.py
@@ -0,0 +1,15 @@
+g=True
+if g:
+    print("g is true")
+    print("end")
+a=False
+if a:
+    print("a is true")
+print("end")
+b=8
+if b==4:
+    print("if is valid")
+elif b==8:
+    print("elif is valid")
+else:
+    print("neither if nor elif")
