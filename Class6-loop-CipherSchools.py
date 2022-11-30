b4ad5326f024901136476f8c2ec97aa3469c3f80
akriti-star <sharmaakriti8678@gmail.com>
2022-11-30 22:54:49 +0530

    project

---
+++ Class6-loop-CipherSchools.py
@@ -0,0 +1,31 @@
+name= "Akriti"
+name.__iter__
+print(type(name))
+print("****")
+for c in  name:
+
+    print(c)
+    print(type(c))
+
+
+for i in range (5):
+    print(i)
+    if i ==3:
+        break
+
+for i in range (4):
+    print(i)
+    i=100
+    print(i)
+
+if True:
+    "vhjiomgdd"
+print("rest of the code")
+   
+
+for i in range (5):
+    print(i)
+    if i==3:
+        break
+else:
+    print("something")
